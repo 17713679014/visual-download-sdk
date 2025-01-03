2f0f443822b6e62e2ed20e6b7d5656162c58464c
xuejintao <xuejintao@baidu.com>
2025-01-03 14:22:41 +0800

    依赖修复

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,36 +1,30 @@
 import typescript from 'rollup-plugin-typescript2';
 import dts from 'rollup-plugin-dts';
-import { defineConfig } from 'rollup';
 
-export default defineConfig([
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: 'dist/index.js',
-        format: 'cjs'
-      },
-      {
-        file: 'dist/index.esm.js',
-        format: 'es'
-      }
-    ],
-    plugins: [
-      typescript({
-        tsconfig: 'tsconfig.build.json',
-        clean: true
-      })
-    ],
-    external: ['axios', 'idb']
-  },
-  {
-    input: 'src/index.ts',
-    output: {
-      file: 'dist/index.d.ts',
-      format: 'es'
+export default [
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: 'dist/index.js',
+                format: 'cjs'
+            },
+            {
+                file: 'dist/index.esm.js',
+                format: 'es'
+            }
+        ],
+        plugins: [
+            typescript({
+                tsconfig: './tsconfig.json',
+                clean: true
+            })
+        ],
+        external: ['axios', 'idb']
     },
-    plugins: [dts({
-      tsconfig: 'tsconfig.build.json'
-    })]
-  }
-]); 
+    {
+        input: 'src/index.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'es' }],
+        plugins: [dts()]
+    }
+]; 
